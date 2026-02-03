3464b49e43cc14eaf80031de796ea48b4057ca39
ABINAS-cyber <royalabinashdash2007@gmail.com>
2026-02-03 11:24:41 +0530

    certain changes

--- DSA/heapsort.cpp
+++ DSA/heapsort.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 int maxheapify(int arr[],int n,int i){
     int largest=i;
-    int l=2*i+1;
-    int r=2*i+2;
+    int l=2*i;
+    int r=2*i+1;
     if(l<n && arr[l]>arr[largest]){
         largest=l;
     }
