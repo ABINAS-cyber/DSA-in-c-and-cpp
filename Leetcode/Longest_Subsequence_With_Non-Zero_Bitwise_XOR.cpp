d0a2d6d5e6016f514abdc65b7dd1cc95dba5625c
ABINAS-cyber <royalabinashdash2007@gmail.com>
2026-08-18 08:02:07 +0530

    implementation of lswnzbxor with optimal solution

--- Leetcode/Longest_Subsequence_With_Non-Zero_Bitwise_XOR.cpp
+++ Leetcode/Longest_Subsequence_With_Non-Zero_Bitwise_XOR.cpp
@@ -1,6 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    
+
+
+// brute approach
+int longestSubsequence(vector<int>& nums) {
+    int n = nums.size();
+    int totalXor = 0;
+    bool allZero = true;
+
+    for (int x : nums) {
+        totalXor ^= x;
+        if (x > 0) {
+            allZero = false;
+        }
+    }
+
+    if (totalXor > 0) {
+        return n;
+    }
+
+    return allZero ? 0 : n - 1;
+}
+
+
+// optimal approach
+int longestSubsequence(vector<int>& nums) {
+        auto tot = 0, nonZero = 0;
+
+        for (auto& n : nums) {
+            nonZero |= n > 0;
+            tot ^= n;
+        }
+
+        return nonZero * (nums.size() - !tot);
+    }
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cout << "Enter number of test cases: ";
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cout << "\nEnter number of elements: ";
+        cin >> n;
+
+        vector<int> nums(n);
+        cout << "Enter array elements: ";
+        for (int i = 0; i < n; i++) {
+            cin >> nums[i];
+        }
+
+        int result = longestSubsequence(nums);
+        cout << "Longest subsequence length = " << result << "\n";
+    }
+
     return 0;
 }
